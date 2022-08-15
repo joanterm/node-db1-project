bc19ef609b4700cfbec8d3d464f1854b6ccbb5ab
joanterm <jo_joanarg@yahoo.com>
2022-08-15 09:33:52 -0400

    Router connected and GET request works

--- api/server.js
+++ api/server.js
@@ -4,4 +4,13 @@ const server = express();
 
 server.use(express.json());
 
+//MIDDLEWARE
+const router = require("./accounts/accounts-router")
+server.use("/api/accounts", router)
+
+//SANITY CHECK
+server.get("/", (req, res) => {
+    res.send("Express is working correctly!")
+})
+
 module.exports = server;
