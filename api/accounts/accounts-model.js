5aabd3823472d7726d19abdec033abc4f2fa4f0e
joanterm <jo_joanarg@yahoo.com>
2022-08-15 16:39:33 -0400

    CRUD functionality works

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,10 +1,12 @@
 const db = require("../../data/db-config")
 
+//GET
 const getAll = () => {
   // DO YOUR MAGIC
   return db("accounts");
 }
 
+//GET BY ID
 const getById = id => {
   // DO YOUR MAGIC
   return db("accounts")
@@ -12,16 +14,33 @@ const getById = id => {
   .first();
 }
 
+//POST
 const create = account => {
   // DO YOUR MAGIC
+  return db("accounts")
+  .insert(account)
+  .then(id => {
+    return getById(id[0]);
+  });
 }
 
+//PUT
 const updateById = (id, account) => {
   // DO YOUR MAGIC
+  return db("accounts")
+  .where("id", id)
+  .update(account)
+  .then(() => {
+    return getById(id);
+  });
 }
 
+//DELETE
 const deleteById = id => {
   // DO YOUR MAGIC
+  return db("accounts")
+  .where("id", id)
+  .del()
 }
 
 module.exports = {
--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const Accounts = require("./accounts-model")
 
+//GET
 router.get('/', (req, res, next) => {
   // DO YOUR MAGIC
   Accounts.getAll()
@@ -12,12 +13,14 @@ router.get('/', (req, res, next) => {
   }))
 })
 
+//GET BY ID
 router.get('/:id', (req, res, next) => {
   // DO YOUR MAGIC
   Accounts.getById(req.params.id)
   .then((result) => {
     if(!result) {
       res.status(404).json({ message: "account not found" })
+      return
     }
     res.status(200).json(result)
   })
@@ -26,17 +29,84 @@ router.get('/:id', (req, res, next) => {
   }))
 })
 
+//POST
 router.post('/', (req, res, next) => {
   // DO YOUR MAGIC
+  if(req.body.name == null || req.body.budget == null) {
+    res.status(400).json({message: "name and budget are required" })
+    return
+  } else if (typeof req.body.name !== "string" || req.body.name.trim() === "" || req.body.name.trim().length < 3 || req.body.name.trim().length > 100) {
+    res.status(400).json({message: "name of account must be between 3 and 100"})
+    return
+  } else if (typeof req.body.budget !== "number") {
+    res.status(400).json({message: "budget of account must be a number"})
+    return
+  } else if (req.body.budget < 0 || req.body.budget > 1000000) {
+    res.status(400).json({message: "budget of account is too large or too small"})
+    return
+  }
+  Accounts.create(req.body)
+  .then((item) => {
+    return Accounts.getById(item.id)
+  })
+  .then((result) => {
+    res.status(201).json(result)
+  })
+  .catch((err => {
+    console.log(err);    
+  }))
 })
 
+//PUT
 router.put('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+  if(req.body.name == null || req.body.budget == null) {
+    res.status(400).json({message: "name and budget are required" })
+    return
+  } else if (typeof req.body.name !== "string" || req.body.name.trim() === "" || req.body.name.trim().length < 3 || req.body.name.trim().length > 100) {
+    res.status(400).json({message: "name of account must be between 3 and 100"})
+    return
+  } else if (typeof req.body.budget !== "number") {
+    res.status(400).json({message: "budget of account must be a number"})
+    return
+  } else if (req.body.budget < 0 || req.body.budget > 1000000) {
+    res.status(400).json({message: "budget of account is too large or too small"})
+    return
+  }
+  Accounts.updateById(req.params.id, req.body)
+  .then(() => {
+    return Accounts.getById(req.params.id)
+  })
+  .then((result) => {
+    if(!result) {
+      res.status(404).json({ message: "account not found" })
+      return
+    }
+    res.status(200).json(result)
+  })
+  .catch((err => {
+    console.log(err);    
+  }))
 });
 
+//DELETE
 router.delete('/:id', (req, res, next) => {
   // DO YOUR MAGIC
+  Accounts.getById(req.params.id)
+  .then((result) => {
+    if(result == null) {
+      res.status(404).json({ message: "account not found" })
+      return
+    }
+    Accounts.deleteById(req.params.id)
+    .then(() => {
+      res.status(200).json(result)
+    })   
+  })
+  .catch((err => {
+    console.log(err);    
+  }))
 })
+
 
 router.use((err, req, res, next) => { // eslint-disable-line
   // DO YOUR MAGIC
